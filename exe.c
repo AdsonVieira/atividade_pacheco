155497fbdda3522aec6f0cdcc2e692ba291babed
Adson Vieira <adsonvieiira@gmail.com>
2016-09-30 07:36:48 -0300

    Atividade quase finalizada

--- exe.c
+++ exe.c
@@ -2,76 +2,63 @@
 #include<stdlib.h>
 #include <locale.h>
 int main(){
-	
-	setlocale(LC_ALL, NULL); 
-	
+
+	  setlocale(LC_ALL,"Portuguese");
+
 	int a=0, b=0, n_maiusc =0, n_minus=0;
 	int size_string=0, l_maius=0, l_minus=0, coun_n=0, anagrama=0, pal=0;
 	int c_especiais=0, size_string_t=0, aux=0;
 
-	char frase_qualquer[100];
+	char frase_qualquer[100], frase_minus[100],frase_maius[100];
 	char outra_frase[100];
-	char resposta = 's';
-	
+
+	frase_qualquer[0] = '\0';
+	outra_frase[0] = '\0';
+
 	int vogais_maiusculas[5] = {65, 69, 73, 79, 85};
 	int vogais_minusculas[5] = {97, 101, 105, 111, 117};
-	
-	int ch_especiais[32] = {33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126};
-	
-	
 
-    
-		inicio:
+
+
 		printf("Digite uma frase qualquer !\n");
 		gets(frase_qualquer);
-		
-	
+
+
 		while(frase_qualquer[a] != '\0'){
 			size_string++;
 			a++;
 		}
 		printf("Tamanho da string %d\n", size_string);
-		
-	
-		
+
+
+
 		printf("\nString invertida :");
-		for(a = size_string -1; a >= 0; a--){
+		for(a = size_string ; a > 0; a--){
 	       	printf("% c", frase_qualquer[a]);
 		}
-		
-	
-		printf("\nFrase maiúscula :");
-	
-		for(a =0; a <= size_string -1; a++){
-			//transforma a string em maiúscula
-			 if(frase_qualquer[a] >= 'A' && frase_qualquer[a] <= 'Z'){
-			 	printf("%c", frase_qualquer[a]);
-			 }else if(frase_qualquer[a] >= 'a' && frase_qualquer[a] <= 'z'){
-			 
-			    printf("%c", frase_qualquer[a] -32);
-		     }else{
-		    	printf("%c", frase_qualquer[a]);
-		    	
-			 }
-		    
+
+
+
+		for(a =0; a <=size_string; a++){
+			// Transformando a frase em maiúsculas
+			if((frase_qualquer[a] >=0 && frase_qualquer[a] <= 64) || (frase_qualquer[a] >=65 && frase_qualquer[a] <= 90)){
+				frase_maius[a]= frase_qualquer[a];
+			}else if(frase_qualquer[a] >=97 && frase_qualquer[a] <= 122){
+				frase_maius[a]= frase_qualquer[a] -32;
+			}
 		}
-		
-		printf("\nFrase minúscula :");
-		for(a =0; a <= size_string -1; a++){
-			//transforma a string em minúscula
-			 if(frase_qualquer[a] < 'Z' && frase_qualquer[a] >= 'A'){
-			 	printf("%c", frase_qualquer[a] + 32);
-			 }else if(frase_qualquer[a] >= 'a' && frase_qualquer[a] <= 'z'){
-			 
-			    printf("%c", frase_qualquer[a]);
-		    }else{
-				printf("%c", frase_qualquer[a]);
-			 }
-		    
+
+		for(a =0; a <=size_string; a++){
+			// Transformando a frase em minúscula
+			if((frase_qualquer[a] >=0 && frase_qualquer[a] <= 64) || (frase_qualquer[a] >=97 && frase_qualquer[a] <= 122)){
+				frase_minus[a]= frase_qualquer[a];
+			}else if(frase_qualquer[a] >=65 && frase_qualquer[a] <= 90){
+				frase_minus[a]= frase_qualquer[a] +32;
+			}
 		}
-			 
-			 
-		for(a =0; a <= size_string ; a++){
+
+
+		for(a =0; a < size_string ; a++){
 			// conta o número de vogais minusculas
 			if(frase_qualquer[a] >= 65 && frase_qualquer[a] <= 90){
 				for(b = 0; b <= 4; b++){
@@ -86,79 +73,67 @@ int main(){
 					}
 				}
 			}
-			
+
 		}
-		printf("\nQuantidade de vogais maiúsculas %d e vogais minúsculas %d\n", n_maiusc,n_minus);
-		
-		for(a =0; a <= size_string;a++){
-			if(frase_qualquer[a] >= 65 &&  frase_qualquer[a] <= 90){
-				l_maius++;
-			}
-			//verifica a quantidade de letras maiúsculas
-		}
-		printf("Quantidade de letras maiúsculas %d\n", l_maius );
-		
-		
-		for(a =0; a <= size_string ;a++){
-			if(frase_qualquer[a] >= 97 &&  frase_qualquer[a] <= 122){
-				l_minus++;
-			}
-			//verifica a quantidade de letras minsculas
-		}
-		printf("Quantidade de letras minúsculas %d\n", l_minus);
-		
-		
-		
-		for(a =0; a <= size_string;a++){
-			if(frase_qualquer[a] >= 48 &&  frase_qualquer[a] <= 57){
+
+
+		// Esse bloco de comando verifica o número de caracteres especiais letras maiúsculas e minúscula da string
+		for(a=0;a <= size_string;a++){
+			if(frase_qualquer[a]>=48 && frase_qualquer[a]<=57){
+				//números
 				coun_n++;
+			}else if(frase_qualquer[a]>= 'A' && frase_qualquer[a]<= 'Z'){
+				l_maius++;
+				// letras maiúsculas
+			}else if(frase_qualquer[a]>= 'a' && frase_qualquer[a]<= 'z'){
+				// letras minusculas
+				l_minus++;
+
+			}else if(frase_qualquer[a]){
+				c_especiais++;
 			}
-			//verifica a quantidade de números
 		}
-		printf("Quantidade de números na string %d\n", coun_n);
-		
-		for(a =0; a <= size_string -1; a++){
-			// verificando número de caracteres especiais
-			for(b = 0; b <= 31; b++){
-				if(frase_qualquer[a] == ch_especiais[b]){
-					c_especiais++;
-				}
-			}
-		
-		}	
-		printf("Quantidade de caracteres especiais encontrados na string: %d\n", c_especiais);
-		
-		for(a=0, b=size_string -1; a <=size_string, b >=0; a++, b--){
+
+		printf("\nFrase maiúscula: %s", frase_maius);
+		printf("\nFrase minúsculas: %s", frase_minus);
+		printf("\nQuantidade de vogais maiúsculas %d e vogais minúsculas %d\n", n_maiusc,n_minus);
+		printf("Quantidade de letras maiúsculas: %d\n", l_maius);
+		printf("Quantidade de letras minúsculas: %d\n", l_minus);
+		printf("Quantidade de números: %d\n", coun_n);
+		printf("Quantidade de caracteres especiais: %d\n", c_especiais);
+
+
+
+
+		for(a=0, b =size_string -1; a < size_string, b >=0; a++, b--){
 			// verificando se é um palídromo
 		   if(frase_qualquer[a] == frase_qualquer[b]){
 		   		pal++;
-		   } 
-		   
+		   }
+
 		}
-		
-		if(pal == size_string -1){
-			printf("é um PALÍNDROMO", setlocale(LC_ALL, "Portuguese"));
+		if(pal == size_string ){
+			printf("é um PALÍNDROMO");
 		}else{
-			printf("Não é um PALÍNDROMO", setlocale(LC_ALL, "Portuguese"));
+			printf("Não é um PALÍNDROMO");
 		}
-		
-		
+
+
 		// Verificando existência de anagrama
-		
+
 		printf("\nDigite outra string: \n");
 		gets(outra_frase);
-		
+
 		for(a =0; outra_frase[a] !='\0'; a++){
 			size_string_t++;
 			// verificando tamanho da segunda string digitada
-		} 
-	    
-	    printf("primeira string %d\n", size_string);
-	    printf("segunda string %d\n", size_string_t);
-	
+		}
+
+
+
 		if(size_string_t == size_string){
-			// ordenando a primeira string  em ordem alfabética 
-			
+			// ordenando a primeira string  em ordem alfabética
+
 			for(a=0; a <= size_string; a++){
 				for(b = a+1; b <= size_string; b++){
 					if(frase_qualquer[a] > frase_qualquer[b]){
@@ -168,7 +143,7 @@ int main(){
 					}
 				}
 			}
-			// ordenando a segunda string  em ordem alfabética 
+			// ordenando a segunda string  em ordem alfabética
 			for(a=0; a <= size_string; a++){
 				for(b = a+1; b <= size_string; b++){
 					if(outra_frase[a] > outra_frase[b]){
@@ -178,36 +153,28 @@ int main(){
 					}
 				}
 			}
-			
+
 			for(a=0; a<= size_string; a++){
 				for(b=0; b <= size_string; b++){
 					if(frase_qualquer[a] == outra_frase[b]){
-						anagrama++;	
+						anagrama++;
 					}
 				}
 			}
-				
+
 			if(anagrama >= size_string && anagrama >=size_string_t){
 				printf("SÃO ANAGRAMAS UM DO OUTRO!.\n ");
 			}else{
-			
+
 				printf("Não são anagramas\n");
-			
+
 			}
-			 
+
 		}else{
-			
-			printf("Não são anagramas\n");		
+
+			printf("Não são anagramas\n");
 		}
-		
-		printf("Deseja continuar- digite s ou n");
-		scanf("%c", &resposta);
-		
-		if(resposta == 's'){
-			goto inicio;
-		}else{
-			return 0;
-		}
+
 }
 
 
